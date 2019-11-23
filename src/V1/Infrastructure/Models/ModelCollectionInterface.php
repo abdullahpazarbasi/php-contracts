bd498c6eb47b912c2507156238d25ae7ad6642a5
Abdullah Pazarbaşı <mapazarbasi@hotmail.com>
2019-11-23 21:31:29 +0300

    Model Collection improved

--- src/V1/Infrastructure/Models/ModelCollectionInterface.php
+++ src/V1/Infrastructure/Models/ModelCollectionInterface.php
@@ -21,18 +21,29 @@ interface ModelCollectionInterface extends Collection
      * @return bool
      */
     public function isModelValid($model);
+    
+    /**
+     * @return bool
+     */
+    public function isSequential();
 
     /**
      * @param array|object[] $array
      * @return static
      */
     public function reconstructFromArray($array);
+    
+    /**
+     * @param Collection $collection
+     * @return static
+     */
+    public function reconstructFromCollection(Collection $collection);
 
     /**
-     * @param Closure $p The predicate which returns a boolean value (TRUE or FALSE)
-     *                Example: function ($key, $model) { ... }
-     * @return mixed if the predicate is TRUE then returns the model,
-     *              NULL otherwise
+     * @param Closure $p   The predicate which returns a boolean value (TRUE or FALSE)
+     *                     Example: function ($key, $model) { ... }
+     * @return object|null If the predicate is TRUE then returns the model,
+     *                     NULL otherwise
      */
     public function find(Closure $p);
 
